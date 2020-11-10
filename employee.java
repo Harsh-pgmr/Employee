492e9ef9cac047508cfbb7d9847bd4da92d32d1d
Harsh <73747616+Harsh-pgmr@users.noreply.github.com>
2020-11-10 23:17:46 +0530

    Added part time or full time

--- employee.java
+++ employee.java
@@ -1,26 +1,31 @@
 public class employee {
     public static void main(String arg[]){
         System.out.println("WELCOME to EMPLOYEE WAGE Computation");
-        int empRatePerHr=20;
+        // Constants
+        int empWagePerHr=20;
+        int is_Part_Time=2;
+        int is_Full_Time=1;
+
+        // Variable
         int empHrs=0;
         int empWage=0;
-        double employee=Math.random();
-        if(employee==1)
-        {
-            System.out.println("Employee is Present");
-            empHrs=8;
-            empWage=(empHrs*empRatePerHr);
-            System.out.println("Daily Wage is "+empWage);
+        // Employee check
+        double employee = (int)(( Math.random() * 10 ) % 3);
+        if ( employee == is_Part_Time ) {
+            System.out.println("Part Time  ");
+            empHrs = 4;
         }
-        else
-        {
-            System.out.println("Employee is Absent");
-            empHrs=0;
-            empWage=(empHrs*empRatePerHr);
-            System.out.println("Daily Wage is "+empWage);
-                        }
+        else if (employee == is_Full_Time ) {
+            System.out.println("Full Time ");
+            empHrs = 8;
+        }
+        else {
+            System.out.println("Employee is Absent ");
+            empHrs = 0;
+        }
+            empWage=(empHrs*empWagePerHr);
+            System.out.println("Employee Wage is "+empWage);
     }
 }
-
 
 
