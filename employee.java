1071b33d91edac24bfb49ae6cdbab24d7e0706ae
Harsh <73747616+Harsh-pgmr@users.noreply.github.com>
2020-11-21 21:29:44 +0530

    updated Store the daily wage along with total wage

--- employee.java
+++ employee.java
@@ -1,17 +1,17 @@
 import java.util.*;
-public class employeeWage implements iEmployeeWage
+public class EmpWage implements iEmployeeWage
 {
 	int counter=0;
 	int numberOfCompanies;
 	ArrayList<companyEmployeeWage> companyEmployeeWageList;
-	employeeWage(int n)
+	EmpWage(int n)
 	{
 		this.numberOfCompanies=n;
 		companyEmployeeWageList=new ArrayList<companyEmployeeWage>();
 	}
 	public void addCompany(String company,int wagePerHour,int days,int maxHours)
 	{
-		companyEmployeeWage companyempwage=new companyEmployeeWage(company,wagePerHour,days,maxHours);
+		companyEmployeeWage companyempwage=new companyEmployeeWage(company,ratePerHour,days,maxHours);
 		companyEmployeeWageList.add(companyempwage);
 	}
 	public void computeWages()
@@ -46,10 +46,6 @@ public class employeeWage implements iEmployeeWage
         int result=(int)Math.floor((Math.random()*10)%3);
         return result;
     }
-    public void print(int totalDays,int workHours,int totalHours,int dailySalary,int totalSalary)
-    {
-        System.out.println(totalDays+"\t"+workHours+"\t\t"+totalHours+"\t\t"+dailySalary+"\t\t"+totalSalary);
-    }
     public int employeeWages(companyEmployeeWage companyEmployee)
     {
         int check=0,workHours=0,dailySalary=0,totalSalary=0,totalHours=0,totalDays=0;
@@ -61,8 +57,12 @@ public class employeeWage implements iEmployeeWage
             workHours=getWorkingHours(check);
             totalHours=totalHours+workHours;
             dailySalary=workHours*companyEmployee.wagePerHour;
-            totalSalary=totalHours*companyEmployee.wagePerHour;
-            print(totalDays,workHours,totalHours,dailySalary,totalSalary);
+            companyEmployee.addDailyWage(dailySalary);
+        }
+        totalSalary=totalHours*companyEmployee.wagePerHour;
+        for(int i=0;i<companyEmployee.dailyWage.size();i++)
+        {
+        	System.out.println("Wage on day "+(i+1)+" is "+companyEmployee.dailyWage.get(i));
         }
         return totalSalary;
     }
